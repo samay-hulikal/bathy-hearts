f66066c28f47ac0a7b0ca3af047310514d2528bd
samay-peleglab-dell <samay-peleglab-dell>
2026-02-12 00:56:06 -0700

    Learning basic pandas functions

--- scripts/explore.py
+++ scripts/explore.py
@@ -1,4 +1,17 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
 
-# This is a comment
+# Path to the directory containing the heart rate data
+data_dir = Path("/home/samay/Desktop/Samay/Collective Behavior/Deep sea data/Heart/smoothed_highfreq_for_orit")
+
+# Creating a list of .csv files in the directory
+heart_csv_files = sorted([f for f in data_dir.glob("*.csv") if not f.name.startswith(".")]) # List comprehension necessary to avoid strange ._ORIT_ files
+print(f"Found {len(heart_csv_files)} files")
+
+# Load the file to see what's going on
+df = pd.read_csv(heart_csv_files[0]) # This "reads" the .csv file and creates a DataFrame (pandas' main data structure)
+print(type(df))
+print(df.head()) # Shows the first 5 rows of the DataFrame
+print(df.columns.tolist()) # Creates a list of column names as a python list
