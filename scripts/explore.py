1fab3dbb12a615947e33f67ae21e5b67947c2ff6
samay-peleglab-dell <samay-peleglab-dell>
2026-02-16 17:58:31 -0700

    Plotted more resampled data; 10min and/or 1hr seem great

--- scripts/explore.py
+++ scripts/explore.py
@@ -18,26 +18,28 @@ for i, (cl, fls) in enumerate(heart_dict.items()):
 
 """
 Pattern 0: Aq2
-    Start: 2025-11-27
-    End: 2025-12-17
+    Start: 2025-11-27 16:38
+    End: 2025-12-17 14:33
 
 Pattern 1: Aq1
-    Start: 2025-11-27
-    End: 2026-01-05
+    Start: 2025-11-27 16:30
+    End: 2026-01-05 14:38
 
 Pattern 2: Aq2-1
-    Start: 2025-12-12
-    End 2026-01-05
+    Start: 2025-12-12 14:13
+    End 2026-01-05 14:42
 """
 # Plotting the data from the sorted files from start_date to end_date for pattern number pt
 start_date = "2025-12-12 15:00:00"
-end_date = "2025-12-12 23:10:00"
+end_date = "2025-12-22 23:10:00"
 pt = 2
+# resampling_interval = "10min" # 100ms, 1s, 1min, 10min, 1h
+resampling_interval = "1h" # 100ms, 1s, 1min, 10min, 1h
 
 heart_vals = list(heart_dict.items())
 clm, df_full = heart_vals[pt]
 df_chunk = df_full[(df_full["Time"] >= pd.to_datetime(start_date)) & (df_full["Time"] <= pd.to_datetime(end_date))] # Extracting the chunk of time needed
-df = df_chunk.set_index("Time").resample("1min").mean().reset_index() # Resampled by taking the mean every minute
+df = df_chunk.set_index("Time").resample(resampling_interval).mean().reset_index() # Resampled by taking the mean every minute
 
 fig, axes = plt.subplots(len(clm) - 1, 1, figsize = (12, 24), sharex=True) #Plots share x-axis
 for i, ax in enumerate(axes):
@@ -47,7 +49,7 @@ for i, ax in enumerate(axes):
 axes[-1].set_xlabel("Time")
 plt.tight_layout()
 print(f"Plotting pattern {pt}")
-plt.savefig(f"prelim-figs/bathy_pattern_resampled_minute_{pt}_{start_date}_{end_date}.pdf")
+plt.savefig(f"prelim-figs/resampled_{pt}_{resampling_interval}_{start_date}_{end_date}.pdf")
 print("Saved plot!")
 
 plt.close(fig) # Saves memory, apparently
